7b88ce919d2b837e10d2822c9c50538a77b56100
Amlan Beuria <amlanbeuria70@gmail.com>
2026-08-19 14:20:47 +0530

    Update database password in DBConnection.java

--- src/main/java/com/util/connection/DBConnection.java
+++ src/main/java/com/util/connection/DBConnection.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/khaalo?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true&characterEncoding=UTF-8";
     private static final String USER = "root";
-    private static final String PASSWORD = "Amlan@2003";
+    private static final String PASSWORD = "root";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     
     static {
